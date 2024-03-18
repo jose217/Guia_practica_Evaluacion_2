a02d35dca0bfc7602d82b5f6c0b9d278e983295f
JSDVS <joseramp99@hotmail.com>
2024-03-17 22:07:29 -0600

    primeros 2 ejercicios

---
+++ Ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿namespace Ejercicio1
+{
+    internal class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5];
+            int[] array2 = new int[5];
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Digite un numero {0}", i+1);
+                string nIngresado = Console.ReadLine();
+                int numero = 0;
+                if (int.TryParse(nIngresado, out numero))
+                {
+                    array1[i] = numero;
+                    array2[i] = array1[i] * 3;
+                }
+                else {
+                    Console.WriteLine("Error: ingrese un numero");
+                    i--;
+                }
+
+            }
+
+            string textoArray1 = String.Join(", ", array1);
+            string textoArray2 = String.Join(", ", array2);
+            Console.WriteLine("Array 1: "+textoArray1+"\n"+"Array 2: "+textoArray2);
+        }
+    }
+}
---
+++ Ejercicio2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace Ejercicio2
+{
+    internal class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            ArrayList colores = new ArrayList();
+            colores.Add("ROJO");
+            colores.Add("AZUL");
+            colores.Add("MORADO");
+            colores.Add("ROSADO");
+            colores.Add("GRIS");
+            colores.Add("VERDE");
+            colores.Add("BLANCO");
+
+            Console.WriteLine("Ingrese un color: ");
+            text = Console.ReadLine().ToUpper();
+
+            if (colores.Contains(text))
+            {
+                Console.WriteLine("El color ingresado es: {0} y existe en la lista", text);
+                Console.WriteLine("Ingrese un nuevo dato:");
+                string nuevo = Console.ReadLine().ToUpper();
+
+                int i = colores.IndexOf(text);
+                colores[i] = nuevo;
+
+                Console.WriteLine("Datos actualizados:");
+
+                foreach (string color in colores)
+                {
+                    Console.WriteLine(color);
+                }
+            }
+            else {
+                Console.WriteLine("El color " + text + " no existe en la lista");
+                Console.WriteLine("No se realizaron cambios.");
+                Console.WriteLine("Lista de colores: ");
+                foreach (string color in colores)
+                {
+                    Console.WriteLine(color);
+                }
+            }
+        }
+    }
+}
